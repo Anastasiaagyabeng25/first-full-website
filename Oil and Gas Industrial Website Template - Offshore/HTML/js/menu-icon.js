4f958615d86d87f64930035f9fe3d8f43d2beb71
Anastasia25 <Anastasiaagyabeng25@gmail.com>
2024-10-08 19:43:18 +0000

    first commit

---
+++ Oil and Gas Industrial Website Template - Offshore/HTML/js/menu-icon.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-icon").addEventListener("click", function() {
+  var navbar = document.getElementById("navbar");
+  navbar.classList.toggle("open"); // Toggle the "open" class
+});
